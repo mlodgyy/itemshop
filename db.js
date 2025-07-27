4ff05d7a723b84c32f4f94f65a5ef1abaec65fcb
mlodgyy <marcelsidor17@gmail.com>
2025-07-27 13:29:29 +0200

    db.js

--- db.js
+++ db.js
@@ -2,15 +2,17 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
+
+const db = pool.promise();
 
 db.query('SELECT 1')
   .then(() => console.log('Połączenie z bazą działa!'))
   .catch(err => console.error('Błąd połączenia z bazą:', err));
 
-module.exports = pool.promise();
+module.exports = db;
